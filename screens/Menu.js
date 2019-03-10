37d403dd5583141e19f07242132fa02c5f3710c8
Pei Sin <psinl0304@gmail.com>
2019-03-11 01:07:42 +0800

    bottom navigation is added to all pages

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,26 +1,67 @@
-//This is an example code for NavigationDrawer//
-import React, { Component } from 'react';
-//import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
-// import all basic components
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, Button, TouchableOpacity,ScrollView } from 'react-native';
+import firebase from 'react-native-firebase';
 
-export default class Menu extends Component {
-  //Screen1 Component
+export default class Menu extends React.Component {
+  componentDidMount() {
+    const {currentUser} = firebase.auth()
+    this.setState({currentUser})
+  }
+  state = { currentUser: null }
   render() {
+    const { currentUser } = this.state
     return (
-      <View style={styles.MainContainer}>
-        <Text style={{ fontSize: 23 }}> Menu </Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={{position: 'absolute', top: 5, right: 5}}>
+          {currentUser && currentUser.email}!
+        </Text>
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate('Menu')}>
+              <Image
+                source={require('../images/drawer.png')}
+                style={{ width: 25, height: 25, marginLeft: 55 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate('Main')}>
+              <Image
+                source={require('../images/home.png')}
+                style={{ width: 25, height: 25, marginLeft: 55 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate('MyAccount')}>
+              <Image
+                source={require('../images/user.png')}
+                style={{ width: 25, height: 25, marginLeft: 55 }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  MainContainer: {
+  container: {
     flex: 1,
-    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonContainer:{
+    flex: 1,
+    bottom:0,
+    position:'absolute',
+    flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 50,
     justifyContent: 'center',
   },
-});
+  button:{
+    justifyContent:'flex-end',
+    flex:1
+  }
+})
--- screens/MyAccount.js
+++ screens/MyAccount.js
@@ -1,26 +1,76 @@
-//This is an example code for NavigationDrawer//
-import React, { Component } from 'react';
-//import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
-// import all basic components
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, Button, TouchableOpacity,ScrollView } from 'react-native';
+import firebase from 'react-native-firebase';
 
-export default class MyAccount extends Component {
-  //Screen1 Component
+export default class MyAccount extends React.Component {
+  handleLogout = () => {
+    const { email, password } = this.state
+    firebase
+    .auth()
+    .signOut()
+    .then(() => this.props.navigation.navigate('Login'))
+    .catch(error => this.setState({errorMessage:error.message}))
+    console.log('handleLogout')
+  }
+  componentDidMount() {
+    const {currentUser} = firebase.auth()
+    this.setState({currentUser})
+  }
+  state = { currentUser: null }
   render() {
+    const { currentUser } = this.state
     return (
-      <View style={styles.MainContainer}>
-        <Text style={{ fontSize: 23 }}> My Account </Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={{position: 'absolute', top: 5, right: 5}}>
+          {currentUser && currentUser.email}!
+        </Text>
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate('Menu')}>
+              <Image
+                source={require('../images/drawer.png')}
+                style={{ width: 25, height: 25, marginLeft: 55 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate('Main')}>
+              <Image
+                source={require('../images/home.png')}
+                style={{ width: 25, height: 25, marginLeft: 55 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.button}>
+            <TouchableOpacity onPress={()=>this.props.navigation.navigate('MyAccount')}>
+              <Image
+                source={require('../images/user.png')}
+                style={{ width: 25, height: 25, marginLeft: 55 }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  MainContainer: {
+  container: {
     flex: 1,
-    paddingTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonContainer:{
+    flex: 1,
+    bottom:0,
+    position:'absolute',
+    flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 50,
     justifyContent: 'center',
   },
-});
+  button:{
+    justifyContent:'flex-end',
+    flex:1
+  }
+})
